a71272c213a056b6caec80edb10c8d32ef21ba10
siddiquisd <165915444+siddiquisd@users.noreply.github.com>
2024-05-13 23:27:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var cleanestCities = ["Karachi", "islamabad", "lahore", "peshawar"];
+for (var i = 0; i <= 4; i++) {
+    if ("lahore" === cleanestCities[i]) {
+        console.log(cleanestCities[i]);
+        break;
+    }
+}
---
+++ main.ts
@@ -0,0 +1,17 @@
+let cleanestCities = ["Karachi" , "islamabad" ,"lahore" ,"peshawar"];
+for (let i = 0; i <=4; i++){
+
+    if("lahore" === cleanestCities [i]){
+
+     console.log( cleanestCities [i])
+     break;
+
+
+
+
+    }
+
+
+
+
+}
